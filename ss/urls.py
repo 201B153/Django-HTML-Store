f7814426cda3a2c10116a635a4040cd362a4727a
mstomar698 <tomarm698@gmail.com>
2023-05-04 22:46:30 +0530

    Refurbishing setup

--- ss/urls.py
+++ ss/urls.py
@@ -11,15 +11,16 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='shop/', permanent=False)),
+    path('shop/', include('shop.urls')),
     path('admin/', admin.site.urls),
     path("shop/", include("shop.urls")),
-    path("blog/", include("blog.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
